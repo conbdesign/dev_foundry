67bd675cdc3a9c41923e13615f06e172161a8275
dl <444c43@gmail.com>
2012-12-16 00:40:30 -0500

    Deck class stubbed out

---
+++ Blackjack/Blackjack/DL/Deck.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Blackjack
+{
+	public class Deck : IDeck
+	{
+		private List<IPlayingCard> cards;
+			public List<IPlayingCard> Cards 
+			{ 
+				get { return cards; }
+				set	{ cards = value; }
+			}
+
+        	public void Shuffle()
+			{
+			}
+
+        	public IPlayingCard Draw()
+			{
+				return null;
+			}
+
+        	public void Reset()
+			{
+			}
+		public Deck ()
+		{
+
+		}
+	}
+}
+
