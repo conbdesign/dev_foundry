17b5f227c528d9583b7e878dd275628885fdab35
dl <444c43@gmail.com>
2012-12-16 00:57:34 -0500

    fixed deck.cs

--- Blackjack/Blackjack/DL/Deck.cs
+++ Blackjack/Blackjack/DL/Deck.cs
@@ -8,35 +8,13 @@ namespace Blackjack
 	public class Deck : IDeck
 	{
 		private List<IPlayingCard> cards;
-<<<<<<< HEAD
-=======
-
->>>>>>> 151081dc7eda0ab597e17a003457fe94804cf426
 		public List<IPlayingCard> Cards 
 		{ 
 			get { return cards; }
 			set	{ cards = value; }
 		}
-<<<<<<< HEAD
 	
 		public void Shuffle()
-=======
-
-        public void Shuffle()
-		{
-		}
-
-        public IPlayingCard Draw()
-		{
-			return null;
-		}
-
-        public void Reset()
-		{
-		}
-
-		public Deck()
->>>>>>> 151081dc7eda0ab597e17a003457fe94804cf426
 		{
 		}
 	
@@ -48,9 +26,10 @@ namespace Blackjack
 		public void Reset()
 		{
 		}
-	public Deck ()
-	{
-		List<IPlayingCard> cards = new List<IPlayingCard>
+		public Deck ()
+		{
+			List<IPlayingCard> cards = new List<IPlayingCard>();
+			
 		}
 	}
 }
