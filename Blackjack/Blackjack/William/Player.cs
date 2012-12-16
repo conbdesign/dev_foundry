5d2ad17f1aecf7f210556de4585f3faa65818ef5
hellsbells <walrusman2@gmail.com>
2012-12-16 00:17:02 -0500

    iadding initial player code

---
+++ Blackjack/Blackjack/William/Player.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Blackjack
+{
+    class Player
+    {
+        private IHand hand;
+        private string name;
+        private PlayerType type;
+
+        public PlayerType Type
+        {
+            get { return type; }
+        }
+        public string Name
+        {
+            get { return name;}
+        }
+
+        public  IHand Hand
+        { 
+            get {return hand;}
+        }
+
+        public Action Play()
+        {
+            return Action.STAND;
+ 
+        }
+        public Player(PlayerType stype, string sname)
+        {
+            stype = Type;
+            sname = Name;
+        }
+    }
+}
